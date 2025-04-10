e5eaeb96ba579d43e4352924d2c1396fc6ac7988
abdullahiqbal07 <abdullahiqbal599@gmail.com>
2025-04-10 21:15:17 +0500

    create boiler for website

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense, lazy } from 'react';
+import { BrowserRouter, Navigate, Outlet, Route, Routes, useLocation } from 'react-router-dom';
+// import { authenticate } from './context/AuthContext';
+
+// import { useContext } from 'react';
+import { useEffect } from 'react';
+
+
+// Lazy loading components
+const NotFound = lazy(() => import('./pages/NotFound'));
+const Home = lazy(()=> import('./pages/Home'))
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isDataLoading, setIsDataLoading] = useState(true);
+
+
+  // const ProtectedRoute = ({ Children }) => {
+  //   // handle it from backend
+  //   const user = localStorage.getItem('token')
+  //   const location = useLocation(); // Get the current URL
+  //   // console.log(user)
+  //   if(!user) {
+  //     return <Navigate to="/signin" replace state={{ from: location }}/>
+  //   }
+  //   return (
+  //     <>
+  //       <Toolbar />
+  //       <NavBar style={{ position: "fixed" }} />
+  //       <Outlet />
+  //     </>
+  //   );
+  // }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    // i want to home after sign in
+
+    <Suspense fallback=".....loading">
+      <BrowserRouter>
+        {/* <NavBar style={{position: "fixed"}}/> */}
+        <Routes>
+          {/* <Route path='/' element={<ProtectedRoute />}> */}
+          {/* </Route> */}
+          {/* <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} /> */}
+          
+          <Route path='/home' element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   )
 }
 
